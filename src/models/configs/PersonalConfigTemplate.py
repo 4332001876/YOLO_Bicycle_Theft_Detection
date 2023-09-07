8177ae7f1c07c64f3f9baa9c1ceb45192b769fe3
4332001876 <mzfslhm@163.com>
2023-09-07 11:32:52 +0800

    linux-cpu release

--- src/models/configs/PersonalConfigTemplate.py
+++ src/models/configs/PersonalConfigTemplate.py
@@ -1,5 +1,5 @@
 class PersonalConfig:
     def __init__(self):
-        self.bike_person_dataset_dir = 'D:/Python/Great Project/YOLO_Bicycle_Theft_Detection_Attachment/BikePerson'
+        self.bike_person_dataset_dir = '/home/aistudio/my_project/YOLO_Bicycle_Theft_Detection_Attach/BikePersonTar/BikePerson'
     def get_bike_person_dataset_dir(self):
         return self.bike_person_dataset_dir

263972e5c457c14e1a57b85c955ca68b9010327e
unakar <xqs2002@mail.ustc.edu.cn>
2023-09-28 16:46:42 +0800

    update config

--- src/server/config.py
+++ src/server/config.py
@@ -4,10 +4,8 @@ import os
 MILVUS_HOST = os.getenv("MILVUS_HOST", "10.0.0.31")
 MILVUS_PORT = int(os.getenv("MILVUS_PORT", "19530"))
 VECTOR_DIMENSION = int(os.getenv("VECTOR_DIMENSION", "2048"))
-INDEX_FILE_SIZE = int(os.getenv("INDEX_FILE_SIZE", "1024"))
 METRIC_TYPE = os.getenv("METRIC_TYPE", "L2")
 DEFAULT_TABLE = os.getenv("DEFAULT_TABLE", "milvus_img_search")
-ERP_MIlVUS_TABLE = os.getenv("ERP_MIlVUS_TABLE", "milvus_erp_img_search")
 TOP_K = int(os.getenv("TOP_K", "10"))
 
 ############### MySQL Configuration ###############

b95eba838143ff8aab8eb22474f42ade7e4160aa
unakar <xqs2002@mail.ustc.edu.cn>
2023-10-12 00:07:08 +0800

    进一步精简

--- src/server/operations.py
+++ src/server/operations.py
@@ -3,8 +3,6 @@
 
 from config import *
 from image_utils import obj_decode, obj_encode
-import base64
-import pickle
 
 def do_create(table, milvus_client, mysql_cli):
     table_name = table
@@ -18,7 +16,7 @@ def do_create(table, milvus_client, mysql_cli):
         raise e
     
 
-
+#TODO:下面这两个函数貌似没用...
 #将 MySQL 数据库中未上传的特征数据更新到 Milvus 中
 def do_load(table, milvus_client, mysql_cli):
     table_name = table
@@ -43,36 +41,13 @@ def do_load_once(table_name, milvus_client, mysql_cli):
             if len(ids) > 0:
                 milvus_client.delete(table_name, "id in [%s]" % ids[0])
             raise e
-        
 
 
-def do_update(uploadImagesModel, img_path, model, milvus_client, mysql_cli):
-    table_name = uploadImagesModel.table
+#TODO: search的
+def do_search(table_name, img_path, top_k, milvus_client, mysql_cli):
     if not table_name:
         table_name = DEFAULT_TABLE
-    # 删除原有milvus 数据
-    ms_data = mysql_cli.search_by_ids( [uploadImagesModel.id],table_name)
-    if len(ms_data) == 0:
-        raise Exception("id not exist")
-    milvus_client.delete(table_name, "id in [%s]" % ms_data[0][1])
-
-    feat = model.resnet50_extract_feat(img_path)
-    ids = milvus_client.insert(table_name, [feat])
-    # milvus_client.create_index(table_name)
-
-    try:
-        # mysql_cli.create_mysql_table(table_name)
-        return mysql_cli.update(table_name, (ids[0], uploadImagesModel.tags, uploadImagesModel.brief,base64.b64encode(pickle.dumps(feat)).decode(),uploadImagesModel.id))
-    except Exception as e:
-        milvus_client.delete(table_name, "id in [%s]" % ids[0])
-        raise e
-    
-
-
-def do_search(table_name, img_path, top_k, model, milvus_client, mysql_cli):
-    if not table_name:
-        table_name = DEFAULT_TABLE
-    feature= model.resnet50_extract_feat(img_path)
+    feature= get_embedding(img_path)
     vectors = milvus_client.search_vectors(table_name, [feature], top_k)
     res = []
     if len(vectors[0]) == 0:
@@ -93,29 +68,6 @@ def do_search(table_name, img_path, top_k, model, milvus_client, mysql_cli):
 
 
 
-def do_upload(uploadImagesModel, img_path, model, milvus_client, mysql_cli):
-    table_name = uploadImagesModel.table
-    if not table_name:
-        table_name = DEFAULT_TABLE
-    feat = model.resnet50_extract_feat(img_path)
-    ids = milvus_client.insert(table_name, [feat])
-    # milvus_client.create_index(table_name)
-    try:
-        # mysql_cli.create_mysql_table(table_name)
-        return mysql_cli.insert(table_name, (ids[0],uploadImagesModel.tags, uploadImagesModel.brief,base64.b64encode(pickle.dumps(feat)).decode()))
-    except Exception as e:
-        raise e
-    
-
-# 删除数据库中的表
-def do_drop(table_name, milvus_cli, mysql_cli):
-    if not table_name:
-        table_name = DEFAULT_TABLE
-    if not milvus_cli.has_collection(table_name):
-        return f"Milvus doesn't have a collection named {table_name}"
-    status = milvus_cli.delete_collection(table_name)
-    mysql_cli.delete_table(table_name)
-    return status
 
 def do_delete(id, table_name, milvus_cli, mysql_cli):
     if not table_name:
@@ -127,3 +79,16 @@ def do_delete(id, table_name, milvus_cli, mysql_cli):
     milvus_cli.delete(table_name, "id in [%s]" % ms_data[0][1])
     mysql_cli.delete_by_id(table_name, id)
     return "ok"
+
+
+def do_count(table_name, milvus_cli):
+    if not table_name:
+        table_name = DEFAULT_TABLE
+    try:
+        if not milvus_cli.has_collection(table_name):
+            return None
+        num = milvus_cli.count(table_name)
+        return num
+    except Exception as e:
+        # sys.exit(1)
+        raise e

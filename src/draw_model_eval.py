81ecc929e80961a3799d8c94159a19573c788404
unakar <xqs2002@mail.ustc.edu.cn>
2023-10-13 11:20:23 +0800

    model_eval

--- src/draw_model_eval.py
+++ src/draw_model_eval.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 # 模型评估结果数据
 data = [
     {
-        'model': '预训练Resnet-18模型直接用于人车对',
+        'model': 'Resnet-18',
         'rank1': 0.12,
         'rank5': 0.65,
         'rank10': 1.22,
@@ -12,7 +12,7 @@ data = [
         'metric': 0.15
     },
     {
-        'model': 'VehicleID模型直接用于人车对',
+        'model': 'VehicleID',
         'rank1': 0.97,
         'rank5': 2.47,
         'rank10': 3.71,
@@ -21,7 +21,7 @@ data = [
         'metric': 0.78
     },
     {
-        'model': 'Market1501 People Re-ID模型直接用于人车对',
+        'model': 'People-Reid(without training)',
         'rank1': 0.93,
         'rank5': 2.55,
         'rank10': 3.80,
@@ -30,7 +30,7 @@ data = [
         'metric': 0.77
     },
     {
-        'model': 'Market1501 People Re-ID模型训练后用于人车对',
+        'model': 'People-Reid(trained)',
         'rank1': 67.95,
         'rank5': 81.73,
         'rank10': 88.68,
@@ -39,7 +39,7 @@ data = [
         'metric': 67.33
     },
     {
-        'model': 'Market1501 People Re-ID模型训练后用于自行车',
+        'model': 'Bike-Reid(Trained)',
         'rank1': 56.25,
         'rank5': 71.43,
         'rank10': 79.53,
@@ -53,7 +53,7 @@ data = [
 x = ['rank1', 'rank5', 'rank10', 'mAP', 'mINP', 'metric']
 models = [d['model'] for d in data]
 y_values = {
-    model: [d[key] for d in data] for key, model in zip(x, models)
+    model: [d[key] for key in x] for model, d in zip(models, data)
 }
 
 # 绘制折线图
@@ -62,7 +62,7 @@ for model, values in y_values.items():
 
 # 设置图例和标题
 plt.legend()
-plt.title('模型性能指标对比')
+plt.title('Model Evaluation')
 
 # 显示折线图
 plt.tight_layout()
